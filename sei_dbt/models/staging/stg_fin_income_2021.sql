eb21ee2df8b1eb7787e67a9d24ba069bedac3e72
Duan <Jon.Duan@gov.bc.ca>
2024-10-03 12:07:33 -0700

    add a  `on-run-end` hook to drop all staging tables with the `stg_` prefix, but does not work very well. Staging tables/views are still in the dev db.

--- sei_dbt/models/staging/stg_fin_income_2021.sql
+++ sei_dbt/models/staging/stg_fin_income_2021.sql
@@ -1,6 +1,6 @@
 
 -- Load raw sales data from the CSV file
-{{ log(target.external_root , info=True) }}
+
 SELECT tax_year, postal_code, cast(median_income AS DOUBLE) AS median_income FROM
 -- {{ source('raw_csv_data', 'fin_neighborhood_incomes_2021') }}
 read_csv_auto('{{ target.external_root }}/FIN - Neighborhood Incomes 2021.csv')
