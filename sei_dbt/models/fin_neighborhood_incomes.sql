5c34bf20bdc7baf634131368d88e03b46cc9ab61
Duan <Jon.Duan@gov.bc.ca>
2024-10-07 16:49:17 -0700

    Previous merge may remove two model files. Recreate FCT_Health_client and fin_neighborhood_incomes models.

--- sei_dbt/models/fin_neighborhood_incomes.sql
+++ sei_dbt/models/fin_neighborhood_incomes.sql
@@ -1,7 +1,12 @@
 -- models/fin_neighborhood_incomes.sql
 {{ config(materialized='table') }}
 
-SELECT 
-        *
-        -- Add any other transformations here
-FROM {{ ref('stg_fin_income_all') }}
+with fin_2016_2020 as (
+    SELECT * FROM {{ ref('stg_fin_income_2016_2020') }}
+),
+fin_2021 as (
+    SELECT * FROM {{ ref('stg_fin_income_2021') }}
+)
+SELECT * FROM fin_2016_2020
+UNION ALL
+SELECT * FROM fin_2021
