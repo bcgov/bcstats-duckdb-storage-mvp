63e5a0e7705fc6bc9a9bf46a11999f67ca407768
thister <tim.hister@gov.bc.ca>
2024-10-30 16:45:24 -0700

    ready to launch the big run

--- _targets.R
+++ _targets.R
@@ -1,8 +1,16 @@
-# Note: the resultant targets have the form `results_{method}_{source}_{csv_id}_{iteration}`
-# So, for example, the object called `results_duckDB_LAN_1_2` has the results for using  duckDB, from the LAN, on CSV #1, with iteration #2.
+# Note: the resultant targets either have the form
+#
+# `read_csv_{source}_{csv_id}_{iteration}`
+#
+# or
+# `duckDB_{db-location}_{source}_{csv_id}_{iteration}`
+
+# So, for example, the object called `duckDB_OneDrive_LAN_1_2` stores the results for making a duckDB database that's saved on OneDrive, populated from csv files from the LAN, using CSV #1, and iteration #2. (Phew!)
 
 # Also, note that my use of targets here was not a complete success. A lot of that reason is that making and reading databases is finicky. You will see, for example, a few instances in which I create/remove the file db.duckdb -- this is because I got lots of errors trying to just run the loops and do 'normal' things in R.
 
+# run this if you want to copy the _targets folder over to onedrive
+# fs::dir_copy("_targets", params$paths[['OneDrive']], overwrite = T)
 
 pacman::p_load(targets, tarchetypes, tidyverse)
 
@@ -10,21 +18,14 @@ pacman::p_load(targets, tarchetypes, tidyverse)
 tar_source()
 tar_option_set(packages = c("readr", "tidyverse", "purrr", "duckdb", "DBI"))
 
-# make sure there isn't any residual duckDB file
-try(duckdb::dbDisconnect(db, shutdown = TRUE), silent = T)
-if (fs::file_exists("db.duckdb")) {
-  gc()
-  system("rm db.duckdb")
-}
-
 params = list(
 
   n_iterations = 2,
 
-  # a tibble that gives the directory to read from as well as a name (eg LAN or OneDrive)
-  csv_paths = tibble::tribble(~dir, ~source,
-    "G:/Operations/Data Science and Analytics/2024_bcstats_db/csvs", "LAN",
-    "C:/Users/thister/OneDrive - Government of BC/2024-025 Brett and Jon Database Test Warehouse/raw_data", "OneDrive"),
+  paths = c(
+    LAN = "G:/Operations/Data Science and Analytics/2024_bcstats_db",
+    OneDrive = "C:/Users/thister/OneDrive - Government of BC/2024-025 Brett and Jon Database Test Warehouse"
+  ),
 
   # a tibble with the CSVs to read as well as a query to run on them to time
   csvs = tibble::tribble(~csv, ~base_query,
@@ -55,11 +56,17 @@ params = list(
   )
 )
 
-# I wanted to make this a target but it was not possible to iterate over it (using `pattern`) and get reasonable names, so I make it a global object instead
+# I wanted to make this a target but it was not possible to iterate over it (using `pattern`) and get reasonable names for the targets, so I make it a global object instead
 the_plan = get_csvs(params$csv_path, params$csvs) |>
   crossing(iteration = 1:params$n_iterations)
 
 saveRDS(the_plan, "RDS/the_plan.Rds")
+
+
+
+
+
+
 
 # we make two targets, one for `read_csv` and the other for duckDB. In each target, we first read the data and then run the queries. For `read_csv` there are two query runs: one with just "regular" dplyr and the other with `duckplyr`.
 
@@ -68,74 +75,13 @@ list(
     values = the_plan,
     names = c("source", "csv_id", "iteration"),
 
-    tar_target(results_read_csv, {
+    tar_target(read_csv, read_csv_target(path = path, base_query = base_query)),
 
-      # read the dataframe into `df`
-      start = Sys.time()
-      df = readr::read_csv(path, quote = "\"", progress = F, show_col_types = F)
-      end = Sys.time()
-      read_time = difftime(end, start)
+    tar_target(duckDB_local, duckDB_target(dbdir = "speedtest_db.duckdb", path = path, base_query = base_query)),
 
-      # run the queries on df
-      query_time = apply(filter(params$query_additions, !use_db), 1, function(row) {
-        query = make_query(row[['prefix']], base_query, row[['suffix']])
-        start = Sys.time()
-        query(df)
-        end = Sys.time()
-        difftime(end, start)
-      })
+    tar_target(duckDB_LAN, duckDB_target(dbdir = paste0(params$paths[['LAN']], "/db/speedtest_db.duckdb"), path = path, base_query = base_query)),
 
-      return(list(read_time = read_time, query_time = query_time))
-    }),
+    tar_target(duckDB_OneDrive, duckDB_target(dbdir = paste0(params$paths[['OneDrive']], "/db/speedtest_db.duckdb"), path = path, base_query = base_query))
 
-    tar_target(results_duckDB, {
-
-      # just in case the db already exists
-      try(duckdb::dbDisconnect(db, shutdown = TRUE), silent = T)
-      if (fs::file_exists("db.duckdb")) {
-        gc()
-        system("rm db.duckdb")
-      }
-
-      db = duckdb::dbConnect(duckdb::duckdb(), dbdir = "db.duckdb")
-
-      # attempt to create the duckDB db
-      start = Sys.time()
-      success = read_data_with_duckDB(csvs$path, nrow.check = 500)
-      end = Sys.time()
-
-      # sometimes it doesn't work, so re-run but use `nrow.check = Inf`
-      if (!success) {
-
-        # remove the db from previous step
-        try(duckdb::dbDisconnect(db, shutdown = TRUE), silent = T)
-        if (fs::file_exists("db.duckdb")) {
-          gc()
-          system("rm db.duckdb")
-        }
-
-        # read the db with `nrow.check = Inf`
-        db = duckdb::dbConnect(duckdb::duckdb(), dbdir = "db.duckdb")
-        start = Sys.time()
-        duckdb::duckdb_read_csv(conn = db, name = 't1', files = path, header = T, nrow.check = Inf, transaction = T)
-        end = Sys.time()
-      }
-      read_time = difftime(end, start)
-
-      # run the queries on the duckdb db
-      query_time = apply(filter(params$query_additions, use_db), 1, function(row) {
-        query = make_query(row[['prefix']], base_query, row[['suffix']])
-        start = Sys.time()
-        query(db)
-        end = Sys.time()
-        difftime(end, start)
-    })
-
-      duckdb::dbDisconnect(db, shutdown = TRUE)
-      gc()
-      system("rm db.duckdb")
-
-    return(list(success = success, read_time = read_time, query_time = query_time))
-    })
   )
 )
