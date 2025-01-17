8d803bcedc79f7494b82f59402f8c7a268efa3a3
Duan <Jon.Duan@gov.bc.ca>
2025-01-17 11:21:20 -0800

    create clean_street_line function in SQL server

---
+++ R/healthclientfile/clean_street_line.sql
@@ -0,0 +1,45 @@
+USE [HealthFiles_test]
+GO
+/****** Object:  UserDefinedFunction [dbo].[clean_street_line]    Script Date: 1/17/2025 11:19:11 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER FUNCTION [dbo].[clean_street_line] (
+    @street_line NVARCHAR(MAX),
+    @city NVARCHAR(MAX)
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @cleaned NVARCHAR(MAX);
+
+    -- Convert to uppercase
+    SET @cleaned = UPPER(@street_line);
+
+    -- Remove extra whitespace
+    SET @cleaned = LTRIM(RTRIM(@cleaned));
+    SET @cleaned = REPLACE(@cleaned, '  ', ' '); -- Repeatedly remove double spaces
+    WHILE CHARINDEX('  ', @cleaned) > 0
+        SET @cleaned = REPLACE(@cleaned, '  ', ' ');
+
+    -- Remove non-alphanumeric characters
+    SET @cleaned = REPLACE(@cleaned, ',', '');
+    SET @cleaned = REPLACE(@cleaned, '.', '');
+    SET @cleaned = REPLACE(@cleaned, '/', '');
+    SET @cleaned = REPLACE(@cleaned, '-', '');
+    -- Add more REPLACE() calls as needed for other non-alphanumeric characters
+
+    -- Remove "BC" abbreviation if redundant
+    SET @cleaned = REPLACE(@cleaned, ' BC ', ' ');
+
+    -- Remove the city name
+    SET @cleaned = REPLACE(@cleaned, ' ' + UPPER(@city) + ' ', ' ');
+
+    -- Remove extra whitespace again after all replacements
+    SET @cleaned = LTRIM(RTRIM(@cleaned));
+    WHILE CHARINDEX('  ', @cleaned) > 0
+        SET @cleaned = REPLACE(@cleaned, '  ', ' ');
+
+    RETURN @cleaned;
+END;
